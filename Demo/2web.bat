c937b5215fac555de83e3d6175b2f081208a954b
ignatandrei <ignatandrei@yahoo.com>
2018-11-05 08:42:16 +0200

    added messages

--- Demo/2web.bat
+++ Demo/2web.bat
@@ -5,5 +5,5 @@ call ng build --prod --build-optimizer
 copy dist\AngularSimple\*.* dist\
 rmdir dist\AngularSimple /s /q
 node node_modules\angular-cli-ghpages\bin\angular-cli-ghpages
-echo now goto 
+echo now goto https://ignatandrei.github.io/AngNetCoreDemo/
 cd..
