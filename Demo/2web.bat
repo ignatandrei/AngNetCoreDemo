4955c2527cae9bdf82231ad8e092b3baaeb8f39b
ignatandrei <ignatandrei@yahoo.com>
2018-11-05 08:13:01 +0200

    added deploy to gh pages

---
+++ Demo/2web.bat
@@ -0,0 +1,9 @@
+cls
+cd AngularSimple
+rmdir dist /s /q
+call ng build --prod --build-optimizer 
+copy dist\AngularSimple\*.* dist\
+rmdir dist\AngularSimple /s /q
+node node_modules\angular-cli-ghpages\bin\angular-cli-ghpages
+echo now goto 
+cd..
