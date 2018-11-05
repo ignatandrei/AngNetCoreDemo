5804dbb960c28a70f32058ae79bf0106930a8e8b
ignatandrei <ignatandrei@yahoo.com>
2018-11-05 09:45:17 +0200

    cordova

--- Demo/cordova.bat
+++ Demo/cordova.bat
@@ -1,8 +1,9 @@
 cls
 cd AngularSimple
-call ng build --prod --build-optimizer 
-copy dist\AngularSimple\*.*  ..\CordovaSimple\dist\
+rem call ng build --prod --build-optimizer
+copy dist\AngularSimple\*.*  ..\CordovaSimple\www\dist\
 cd..
+pause
 cd CordovaSimple
 cordova run android
 cd ..
